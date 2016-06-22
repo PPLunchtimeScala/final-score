b9780e7f9fc258ddabfe140b9084786cdd66bf39
insdami <damian.albrun@paddypower.com>
2016-06-22 16:52:19 +0100

    Clean up

--- damian/FinalScore.scala
+++ damian/FinalScore.scala
@@ -19,21 +19,6 @@ object Game extends App {
   printForFinalScore(eventAfterEnd)
   printForFinalScore(eventBeforeStart)
 
-  println("FinalScore2")
-  printForFinalScore2(validList)
-  printForFinalScore2(noKickoffElement)
-  printForFinalScore2(typos)
-  printForFinalScore2(noFinalWhistle)
-  printForFinalScore2(eventAfterEnd)
-  printForFinalScore2(eventBeforeStart)
-
-  println("FinalScore3")
-  printForFinalScore3(validList)
-  printForFinalScore3(noKickoffElement)
-  printForFinalScore3(typos)
-  printForFinalScore3(noFinalWhistle)
-  printForFinalScore3(eventAfterEnd)
-  printForFinalScore3(eventBeforeStart)
 
   def printResult(events: List[String])(f: List[String] => String) = {
     val start = System.nanoTime()
@@ -41,8 +26,6 @@ object Game extends App {
   }
 
   def printForFinalScore(events: List[String]) = printResult(events)(FinalScore.process)
-  def printForFinalScore2(events: List[String]) = printResult(events)(FinalScore2.process)
-  def printForFinalScore3(events: List[String]) = printResult(events)(FinalScore3.process)
 
 }
 
@@ -72,59 +55,7 @@ case class Score(home: Int = 0, away: Int = 0) extends Result {
   def show = s"$home - $away"
 }
 
-
 object FinalScore {
-
-  def process(events: List[String]): String = {
-    validate(events) match {
-      case score: Score => sum(events, score).show
-      case invalid => invalid.show
-    }
-  }
-  def sum(events: List[String], currentResult: Result): Result = (events, currentResult) match {
-    case (_, Invalid) => Invalid
-    case (Nil, score) => score
-    case (events, score: Score) => sum(events.tail, score.process(events.head))
-  }
-
-  def validate(events: List[String]): Result = {
-    if(events.head != startGame) Invalid
-    else if(events.last != endGame) Invalid
-    else Score()
-  }
-
-}
-
-object FinalScore2 {
-
-  def process(events: List[String]): String = {
-    def go(e: List[String], result: Result): Result = e match {
-      case Nil => result
-      case x :: xs => go(xs, rule((result, x))
-    }
-    go(events, validate(events)).show
-  }
-
-  def validate(events: List[String]): Result = {
-    if(events.head != startGame) Invalid
-    else if(events.last != endGame) Invalid
-    else Score()
-  }
-
-  val rule = new PartialFunction[(Result, String), Result] {
-    override def isDefinedAt(state: (Result, String)): Boolean = state._2 match {
-      case `startGame` | `endGame` | `goalHome` | `goalAway` => true
-      case _ => false
-    }
-
-    override def apply(state: (Result, String)): Result = state match {
-      case (result, event) => result.process(event)
-    }
-  }
-
-}
-
-object FinalScore3 {
 
   def process(events: List[String]): String = sum(events).show
 
